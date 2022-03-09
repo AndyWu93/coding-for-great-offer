35658986007058178d3ebc350796172adcac4fc5
jaysun197 <815533359@qq.com>
2022-03-09 20:56:07 +0800

    class11 dp逆推所有可能性

--- src/class11/Code01_MinimumInsertionStepsToMakeAStringPalindrome.java
+++ src/class11/Code01_MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -3,6 +3,32 @@ package class11;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 给定一个字符串，如果可以在任意位置添加字符，最少添加几个能让字符串整体都是回文串。
+ * 思路：该题为动态规划，范围尝试的模型
+ * dp[i][j]:从i位置到j位置，补成回文需要几个字符
+ * 对角线含义：0-0位置，1-1位置... 自然就是回文，所以都填0
+ * 对角线往下为i>j没有意义，范围尝试i>=j
+ * 第二条对角线含义：0-1位置，1-2位置... 判断str[i]==str[j]?0:1
+ * 普遍位置：dp[l][r]
+ * 1. l..r-1变成回文，r位置变成回文需要在最前方加上str[r],所以dp[l][r-1]+1
+ * 2. l+1..r变成回文，l位置变成回文需要在最后方加上str[l],所以dp[l+1][r]+1
+ * 3. l+1..r-1变成回文，条件：str[l]==str[r],所以dp[l+1][r-1]
+ *	123取最小，dp[0][n-1]就是题解
+ *
+ * 总结
+ * 范围尝试：一般需要考虑开头和结尾
+ * 一个样本作行，一个样本作列：一般考虑结尾的各种情况
+ *
+ * 进阶：如果要将所有添加最少字符的回文串返回该怎么做？
+ * 首先，将dp表生成，
+ * 来到dp[0][n-1],看下这个结果是123哪个步骤得到的，
+ * 如果是1，那就在结果串res开头加上str[n-1]，再去递归解决dp[0][n-2]
+ * 如果是1，那就在结果串res结尾加上str[0]，再去递归解决dp[1][n-1]
+ * 如果是3，那就在结果串res开头加上str[0],结尾加上str[n-1]，再去递归解决dp[1][n-2]
+ * 直到来到dp[l][r] l==r
+ *
+ */
 // 本题测试链接 : https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
 public class Code01_MinimumInsertionStepsToMakeAStringPalindrome {
 
@@ -19,6 +45,7 @@ public class Code01_MinimumInsertionStepsToMakeAStringPalindrome {
 		}
 		for (int i = N - 3; i >= 0; i--) {
 			for (int j = i + 2; j < N; j++) {
+				/*1，2种可能性先决策*/
 				dp[i][j] = Math.min(dp[i][j - 1], dp[i + 1][j]) + 1;
 				if (str[i] == str[j]) {
 					dp[i][j] = Math.min(dp[i][j], dp[i + 1][j - 1]);
@@ -47,13 +74,16 @@ public class Code01_MinimumInsertionStepsToMakeAStringPalindrome {
 				}
 			}
 		}
-		
+		/*以上是先生成dp，这里往下是生成一种结果*/
 		int L = 0;
 		int R = N - 1;
+		/*收集结果的ans，长度需要加上dp[L][R]，因为dp[L][R]表示最少需要添加的字符*/
 		char[] ans = new char[N + dp[L][R]];
+		/*填写ans的两个指针*/
 		int ansl = 0;
 		int ansr = ans.length - 1;
 		while (L < R) {
+			/*来自可能性1*/
 			if (dp[L][R - 1] == dp[L][R] - 1) {
 				ans[ansl++] = str[R];
 				ans[ansr--] = str[R--];
@@ -65,6 +95,7 @@ public class Code01_MinimumInsertionStepsToMakeAStringPalindrome {
 				ans[ansr--] = str[R--];
 			}
 		}
+		/*这里表示就剩1个字符，天然就是回文*/
 		if (L == R) {
 			ans[ansl] = str[L];
 		}
@@ -112,11 +143,18 @@ public class Code01_MinimumInsertionStepsToMakeAStringPalindrome {
 				path[pr] = str[R];
 				process(str, dp, L, R - 1, path, pl + 1, pr - 1, ans);
 			}
+			/*深度优先遍历，上面一个案例制造完了以后为什么没有清理现场，因为后续可以将之前的值都覆盖了，不影响结果收集*/
 			if (dp[L + 1][R] == dp[L][R] - 1) {
 				path[pl] = str[L];
 				path[pr] = str[L];
 				process(str, dp, L + 1, R, path, pl + 1, pr - 1, ans);
 			}
+			/*
+			* 来自可能性3的判断：
+			* str[L] == str[R]:必须LR位置字符相等才有
+			* L == R - 1：第二条对角线，结合上一个条件就是，LR是最后的两个相等的字符
+			* dp[L + 1][R - 1] == dp[L][R]：LR位置自消化，里面的范围有没有加字符，所以来自可能3是有可能的
+			 * */
 			if (str[L] == str[R] && (L == R - 1 || dp[L + 1][R - 1] == dp[L][R])) {
 				path[pl] = str[L];
 				path[pr] = str[R];
--- src/class11/Code02_PalindromePartitioningII.java
+++ src/class11/Code02_PalindromePartitioningII.java
@@ -3,6 +3,20 @@ package class11;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ *
+ * Return the minimum cuts needed for a palindrome partitioning of s.
+ * 题意：给定字符串str，问最少切几刀能都变成回文
+ * 解题：
+ * 	动态规划，从左往右的尝试模型
+ * 	尝试过程是枚举str[i]开始，一直到某个位置，这一部分是回文，分成1个部分，剩下的去递归得到x个部分。收集最小值
+ * 	根据尝试，得到dp[i]依赖i后面的位置，求max，所以从后往前推
+ * 	其中，关于求str[i..j]位置是不是回文，可以通过另一张dp表求得
+ *
+ * 	另一张dp表：dp[i][j]:str[i..j]是不是回文，是范围尝试的模型
+ *
+ */
 // 本题测试链接 : https://leetcode.com/problems/palindrome-partitioning-ii/
 public class Code02_PalindromePartitioningII {
 
@@ -13,34 +27,49 @@ public class Code02_PalindromePartitioningII {
 		}
 		char[] str = s.toCharArray();
 		int N = str.length;
+		/*生成dp表：dp[i][j]:str[i..j]是否回文*/
 		boolean[][] checkMap = createCheckMap(str, N);
+		/*dp[i]:从str[i]开始往后，可以分成几个部分的回文*/
 		int[] dp = new int[N + 1];
+		/*从N开始往后没有字符，0个部分的回文*/
 		dp[N] = 0;
+		/*
+		* 尝试过程是枚举str[i]开始，一直到某个位置，这一部分是回文，分成1个部分，剩下的去递归得到x个部分。收集最小值
+		* 根据尝试，得到dp[i]依赖i后面的位置，求max，所以从后往前推
+		* 其中，关于求str[i..j]位置是不是回文，可以通过另一张dp表求得
+		* */
 		for (int i = N - 1; i >= 0; i--) {
 			if (checkMap[i][N - 1]) {
+				/*如果这一部分整体是回文，就分成1个部分*/
 				dp[i] = 1;
 			} else {
 				int next = Integer.MAX_VALUE;
+				/*从i往后到j位置，枚举是回文的情况，j往后的str可以分成几个回文直接从表里拿*/
 				for (int j = i; j < N; j++) {
 					if (checkMap[i][j]) {
 						next = Math.min(next, dp[j + 1]);
 					}
 				}
+				/*1就是i到j位置作为1个部分*/
 				dp[i] = 1 + next;
 			}
 		}
+		/*返回切了几刀，是回文部分数-1*/
 		return dp[0] - 1;
 	}
 
 	public static boolean[][] createCheckMap(char[] str, int N) {
 		boolean[][] ans = new boolean[N][N];
 		for (int i = 0; i < N - 1; i++) {
+			/*对角线，以及只有两个字符时的情况一起填写*/
 			ans[i][i] = true;
 			ans[i][i + 1] = str[i] == str[i + 1];
 		}
 		ans[N - 1][N - 1] = true;
+
 		for (int i = N - 3; i >= 0; i--) {
 			for (int j = i + 2; j < N; j++) {
+				/*i位置和j位置相等，且其他位置也是回文*/
 				ans[i][j] = str[i] == str[j] && ans[i + 1][j - 1];
 			}
 		}
@@ -71,11 +100,20 @@ public class Code02_PalindromePartitioningII {
 					dp[i] = 1 + next;
 				}
 			}
+			/*以上都是和上一个问题代码一样*/
 			// dp[i]  (0....5) 回文！  dp[0] == dp[6] + 1
 			//  (0....5)   6
+			/*
+			* 如何回溯？
+			* 通过i，j指针枚举出回文，
+			* 如果i到j是回文，且j往后的回文部分数+1正好是i往后的回文部分数
+			* 表明i到j这里切了1刀，作为了1个部分，先收集到答案里去，再去j往后收集
+			* */
 			for (int i = 0, j = 1; j <= N; j++) {
+				/*两个条件同时成立*/
 				if (checkMap[i][j - 1] && dp[i] == dp[j] + 1) {
 					ans.add(s.substring(i, j));
+					/*i来到j的位置，继续收集后面的*/
 					i = j;
 				}
 			}
@@ -109,6 +147,7 @@ public class Code02_PalindromePartitioningII {
 					dp[i] = 1 + next;
 				}
 			}
+			/*收集所有的就用深度优先遍历*/
 			process(s, 0, 1, checkMap, dp, new ArrayList<>(), ans);
 		}
 		return ans;
@@ -123,6 +162,7 @@ public class Code02_PalindromePartitioningII {
 			if (checkMap[i][j - 1] && dp[i] == dp[j] + 1) {
 				path.add(s.substring(i, j));
 				ans.add(copyStringList(path));
+				/*这里要还原现场*/
 				path.remove(path.size() - 1);
 			}
 		} else {// s[i...j-1]
@@ -131,6 +171,7 @@ public class Code02_PalindromePartitioningII {
 				process(s, j, j + 1, checkMap, dp, path, ans);
 				path.remove(path.size() - 1);
 			}
+			/*i到j+1位置成为一整体的部分继续深度优先遍历讨论*/
 			process(s, i, j + 1, checkMap, dp, path, ans);
 		}
 	}
